61405c7496b905ebdd8ba1f4d7e191a5f6afa77e
Miguel Dias <miguel.dias@estudante.ufjf.br>
2024-01-27 17:28:01 -0300

    mapa criado

--- scripts/jsons/Jogo/Jogo.js
+++ scripts/jsons/Jogo/Jogo.js
@@ -1,9 +1,9 @@
 
 
-import { Idosa } from "../inimigos/Idosa.js";
+import { Idosa } from "../inimigos/Inimigos.js";
 import { getJogador } from "../../Jogador.js";
 import { PorDecknaTela, atualizarValores } from "../../Tela.js";
-
+import { criarMapa } from "../../mapa.js";
 
 const j1 = getJogador();
 export const inimigo = Idosa
@@ -13,7 +13,7 @@ atualizarValores()
 PorDecknaTela(j1.maodoJogador);
 j1.checarVida();    
 
-
+criarMapa();
 export  function cicloJogo(carta){
     
     let cartas = j1.maodoJogador;
---
+++ scripts/mapa.js
@@ -0,0 +1,12 @@
+export function criarMapa(){
+    var mapa = document.createElement("div");
+    document.body.appendChild(mapa);
+    mapa.classList.add("mapa");
+
+    for (let i = 0; i <4; i++) {
+        
+        const element = array[i];
+        
+    }
+    console.log("mapa criado");
+}
