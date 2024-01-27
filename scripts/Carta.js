8be5f64cf64cb1a165a0c4e8723041b9245d9f17
Miguel Dias <miguel.dias@estudante.ufjf.br>
2024-01-26 22:57:43 -0300

    Co-authored-by: CauaMoreno <CauaMoreno@users.noreply.github.com> Co-authored-by: Estêvão Rocha <tetenc555@users.noreply.github.com> função ataque criada

--- scripts/Carta.js
+++ scripts/Carta.js
@@ -1,9 +1,32 @@
-function criarCarta(){
-    let carta ={}
+function criarCarta(_nome, _descricao, _status, _efetividadeCom){
+    const carta ={}
+    /// referencia ao html
 
-    carta.nome = "batata"
+    //variaveis estasticas
+    const nome = _nome
+    const descricao = _descricao
+    const dano = _status.dano
+    const defesa = _status.defesa 
+
+    const efetividadeCom = _efetividadeCom
+    const poucoEfetivoCom = _poucoEfetivoCom
+    // funções
+    function ataque ( inimigo ){
+        muitoefetivo = inimigo.nome in efetividadeCom 
+        poucoEfetivo = inimigo.nome in poucoEfetivoCom
+        if(muitoefetivo){
+            inimigo.graca += ( dano - defesa) * 1.5;
+            return
+        }
+        if(poucoEfetivo){
+            return
+        }
+        inimigo.graca += ( dano - defesa) 
+    }
+
+    carta.nome = nome
     
-    carta.ação = function( player ){
-        player.vida += 1
+    carta.acao = function( player ){
+        player.graca += 1
     }   
 }
