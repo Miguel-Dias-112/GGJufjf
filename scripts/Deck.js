164855ee89f49b41179b4ea8b6907cda1f2179d1
Miguel Dias <miguel.dias@estudante.ufjf.br>
2024-01-27 00:28:08 -0300

    Deck arrumado

--- scripts/Deck.js
+++ scripts/Deck.js
@@ -7,24 +7,23 @@ export function criarDeck( cartas ){
     
     puxarCartas = function(){
         const tamanho = deck.cartas.length;
-        function selecionar() {
-            let randomizadores=[]
-            for(let i = 0; i < 3; i++){
-                randomizadores[i] = Math.floor(Math.random() * (tamanho )) + 1;
-            }
-            
-            while(randomizadores[0] == randomizadores[1] || randomizadores[0] == randomizadores[2] || randomizadores[1] == randomizadores[2]){
-                randomizadores[1]= Math.floor(Math.random() * (tamanho )) + 1;
-                randomizadores[2]= Math.floor(Math.random() * (tamanho )) + 1;
-            }
-            
-            let cartas =[]
-            randomizadores.forEach(randomizador => {
-                cartas.push(deck.cartas[randomizador])
-            });
-
-            return cartas;
+        let randomizadores=[]
+        for(let i = 0; i < 3; i++){
+            randomizadores[i] = Math.floor(Math.random() * (tamanho )) + 1;
         }
+        
+        while(randomizadores[0] == randomizadores[1] || randomizadores[0] == randomizadores[2] || randomizadores[1] == randomizadores[2]){
+            randomizadores[1]= Math.floor(Math.random() * (tamanho )) + 1;
+            randomizadores[2]= Math.floor(Math.random() * (tamanho )) + 1;
+        }
+        
+        let cartas =[]
+        randomizadores.forEach(randomizador => {
+            cartas.push(deck.cartas[randomizador])
+        });
+
+        return cartas;
+        
         
        
         return 
