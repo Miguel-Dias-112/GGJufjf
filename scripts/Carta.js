a0bb11928f756ea6f713921afb692b390c66a35f
Miguel Dias <miguel.dias@estudante.ufjf.br>
2024-01-27 01:48:06 -0300

    nomenclatura arrumada

--- scripts/Carta.js
+++ scripts/Carta.js
@@ -1,20 +1,30 @@
-const carta ={}
-const _dados = JSON.parse( readFile('../jsons/carta.json', 'utf8'));
-carta.dados = _dados[index]
-carta.cartaContainer = document.createElement('div');
-carta.ataque =  function ( inimigo ){
-    muitoefetivo = inimigo.nome in efetividadeCom;
-    poucoEfetivo = inimigo.nome in poucoEfetivoCom;
-    if(muitoefetivo){
-        inimigo.graca += (carta.dano - carta.defesa) * 1.5;
-        return;
+export function pegarCarta(index){
+    const carta ={}
+    /// referencia ao html
+
+    const _dados = JSON.parse( readFile('../jsons/carta.json', 'utf8'));
+    carta.dados = _dados[index]
+
+    carta.cartaContainer = document.createElement('div');
+
+    carta.ataque =  function ( inimigo ){
+
+        muitoefetivo = inimigo.nome in efetividadeCom;
+        poucoEfetivo = inimigo.nome in poucoEfetivoCom;
+        if(muitoefetivo){
+            inimigo.graca += (carta.dano - carta.defesa) * 1.5;
+            return;
+        }
+        if(poucoEfetivo){
+            inimigo.graca -= (carta.dano - carta.defesa) * 0.5;
+            return;
+        }
+        inimigo.graca += (carta.dano - carta.defesa);
+
     }
-    if(poucoEfetivo){
-        inimigo.graca -= (carta.dano - carta.defesa) * 0.5;
-        return;
-    }
-    inimigo.graca += (carta.dano - carta.defesa);
+    
+
+            
+    return carta;
 }
 
-        
-return carta;
