935e319c5fd7c1bbe61e78a096af9da23a9063a7
Miguel Dias <miguel.dias@estudante.ufjf.br>
2024-01-27 01:34:43 -0300

    Assets criados Deck excluido

--- scripts/Jogador.js
+++ scripts/Jogador.js
@@ -2,18 +2,31 @@ function criaJogador(){
     let maodoJogador = [];
 
     
-    
+    function puxarCarta(mao){
+        while(mao.length < 3)
+        {
+            cNova = Math.floor(Math.random() * (tamanho )) + 1;
+            for (let i=0;i<3;i++)
+            {
+                if (cNova === mao[i])
+                {
+                    cNova = Math.floor(Math.random() * (tamanho )) + 1;
+                    i=0;
+                }
+            }
+        }
+    }
     function definirCliquePraDescerMao(){
-        maodoJogador.forEach(mao => {
-            mao.cartaContainer.addEventListener('click', function(){
+        maodoJogador.forEach(carta => {
+            carta.cartaContainer.addEventListener('click', function(){
 
-                const cartaIndex = maodoJogador.indexOf(mao);
+                
+                const cartaIndex = maodoJogador.indexOf(carta);
                 maodoJogador.splice(cartaIndex, 1);
            
 
             });
         });
-
-
     }
+
 }
