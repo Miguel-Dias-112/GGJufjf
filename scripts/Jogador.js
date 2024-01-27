874ee82b6de602c99ceb398773ec517f0884480b
Miguel Dias <miguel.dias@estudante.ufjf.br>
2024-01-27 02:10:33 -0300

    Co-authored-by: CauaMoreno <CauaMoreno@users.noreply.github.com> Co-authored-by: Estêvão Rocha <tetenc555@users.noreply.github.com> funções ajustas e modularizadas corretamente

--- scripts/Jogador.js
+++ scripts/Jogador.js
@@ -1,29 +1,13 @@
+import {pegarCarta} from './Carta.js';
+
+
 function criaJogador(){
     let maodoJogador = [];
+    let vida=4;
+    let alegriaCtn= document.querySelector('#alegria');
+    
 
     
-    function puxarCarta(mao){
-        while(mao.length < 3)
-        {
-            cNova = Math.floor(Math.random() * (tamanho )) + 1;
-            for (let i=0;i<3;i++)
-            {
-                if (cNova === mao[i])
-                {
-                    cNova = Math.floor(Math.random() * (tamanho )) + 1;
-                    i=0;
-                }
-            }
-        }
-    }
-    function definirCliquePraDescerMao(){
-        maodoJogador.forEach(carta => {
-            carta.cartaContainer.addEventListener('click', function(){
-                const cartaIndex = maodoJogador.indexOf(carta);
-                maodoJogador.splice(cartaIndex, 1);
-                porCartaTela(maodoJogador);
-            });
-        });
-    }
+
 
 }
--- scripts/Tela.js
+++ scripts/Tela.js
@@ -1,15 +1,53 @@
-export function porCartaTela(cartas){
+
+
+
     
-    const mesaCartas = document.querySelector('#mesaCartas');
-    function criarContainerCarta(descricao, cartaContainer){
-        let descricaoCtn = document.createElement('p');
-        descricaoCtn.textContent = descricao
-        cartaContainer.classList.add('carta');
-        cartaContainer.appendChild(descricaoCtn);
-        mesaCartas.appendChild(cartaContainer);
+function porMaonaTela(maodoJogador){
+    function porCartaTela(cartas){
+    
+        const mesaCartas = document.querySelector('#mesaCartas');
+        function criarContainerCarta(descricao, cartaContainer){
+            let descricaoCtn = document.createElement('p');
+            descricaoCtn.textContent = descricao
+            cartaContainer.classList.add('carta');
+            cartaContainer.appendChild(descricaoCtn);
+            mesaCartas.appendChild(cartaContainer);
+        }
+        
+        cartas.forEach(carta => {
+            criarContainerCarta(carta.descricao, carta.cartaContainer);
+        });
     }
     
-    cartas.forEach(carta => {
-        criarContainerCarta(carta.descricao, carta.cartaContainer);
+    function puxarCarta(maodoJogador){
+        while(maodoJogador.length < 3)
+        {
+            const totalCarta = 3; // tamanho de cartas do json
+            let cNova = Math.floor(Math.random() * (totalCarta )) + 1;
+            let tamanho = maodoJogador.length;
+
+            for (let i=0;i<tamanho;i++)
+            {
+                if (cNova == mao[i])
+                {
+                    cNova = Math.floor(Math.random() * (tamanho )) + 1;
+                    i=0;
+                }
+            }
+
+            maodoJogador.push(pegarCarta(cNova));
+            
+        }
+
+
+    }
+    maodoJogador.forEach(carta => {
+        carta.cartaContainer.addEventListener('click', function(){
+            const cartaIndex = maodoJogador.indexOf(carta);
+            maodoJogador.splice(cartaIndex, 1);
+
+            puxarCarta(maodoJogador);
+            porCartaTela(maodoJogador);
+        });
     });
 }
